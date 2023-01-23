70e75be7e05cc10de7b3785a78b1d592825ad9df
elecr0n4ik <andreyshka3@gmail.com>
2023-01-23 14:32:05 +0300

    step 1

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-import sqlite3
+from flask import Flask, request, render_template, jsonify, redirect
+from utils import *
 
-con = sqlite3.connect("netflix.db")
-cur = con.cursor()
-sqlite_query = ("select SUM(duration) "
-                "FROM netflix "
-                "WHERE release_year=2010 "
-                "AND `type`='Movie'")
-cur.execute(sqlite_query)
-minutes = cur.fetchall()[0][0]
-hours = minutes // 60
-result = (f'Чтобы посмотреть все фильмы, нам нужно {hours} часов.')
-con.close()
+app = Flask(__name__)
+# Чтобы заработала кириллица
+app.config['JSON_AS_ASCII'] = False
 
-if __name__ == '__main__':
-    print(result)
+@app.route("/movie/<title>")
+def search_page(title):
+    """
+    Вывод информации по названию фильма GET /movie/<title>
+    """
+    return search_movie(title)#jsonify(search_movie(title))
+
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """
+    Представление для ошибки "Страница не найдена"
+    """
+    return "<h1>Страница не найдена</h1>", 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    """
+    Представление для ошибки "Внутренняя ошибка сервера"
+    """
+    return "<h1>Сервер не отвечает</h1>", 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
